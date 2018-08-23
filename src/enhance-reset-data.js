30818fc2f23fa56fb099eb50dea744b52e17435d
Bradley J. Spaulding <brad.spaulding@gmail.com>
2018-08-23 08:37:58 -0700

    Replaced with keynote html export

--- src/enhance-reset-data.js
+++
@@ -1,7 +0,0 @@
-// in a new EnhancedWidgetsReducer.js
-switch (action.type) {
-  DATA_LOADED:
-    return inputReducer(clearWidgets(newState), action);
-  default:
-    return inputReducer(state, action);
-}
